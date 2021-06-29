850c91b38ee28c15569a346b88077cd2d5d2ddef
Romina Schann <cuqi213@gmail.com>
2021-06-29 17:58:32 +0900

    array HW2

--- arrayHw.js
+++ arrayHw.js
@@ -6,7 +6,8 @@ function findName(name){
     // } else{
     //     console.log("없습니다")
     // }
-    let checkIn = false;
+
+    let checkIn = false; //my second answer
     attendance.forEach((i) => {
         if(i == name){
             checkIn = true;
@@ -18,7 +19,27 @@ function findName(name){
     }else{
         console.log("없습니다")
     }
+
 }
 
-findName("흥민");
-findName("명수");
+// findName("흥민");
+// findName("명수");
+
+function multiTable(number){
+    for(var i=1;i<10;i++){
+        console.log(`${number} * ${i} = ${number*i}`);
+    }
+    console.log("\n");
+    
+    // console.log(`${number} * 2 = ${number*2}`);
+    // console.log(`${number} * 3 = ${number*3}`);
+    // console.log(`${number} * 4 = ${number*4}`);
+    // console.log(`${number} * 5 = ${number*5}`);
+    // console.log(`${number} * 6 = ${number*6}`);
+    // console.log(`${number} * 7 = ${number*7}`);
+    // console.log(`${number} * 8 = ${number*8}`);
+    // console.log(`${number} * 9 = ${number*9}\n`); //No more hard coding!
+}
+for(var j=2;j<10;j++){
+    multiTable(j);
+}
